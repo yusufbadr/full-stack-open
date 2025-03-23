ad3d0ffe0a13449a9ad052626d099fe3647744d3
Yusuf Badr <yusufbadr@yahoo.com>
2025-03-23 03:03:10 +0200

    finished part2

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const CountryDetails = ({ country }) => {
+    const [weather, setWeather] = useState(null)
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY
+
+    useEffect(() => {
+        const capital = country.capital[0]
+        const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${apiKey}`
+        axios.get(weatherApiUrl).then(response => {
+            setWeather(response.data)
+        })
+    }, [country, apiKey])
+
     return (
         <div>
             <h2>{country.name.common}</h2>
@@ -13,6 +27,18 @@ const CountryDetails = ({ country }) => {
             </ul>
             <img src={country.flags.png} alt={`Flag of ${country.name.name}`} width="150" />
 
+            {weather && (
+                <div>
+                    <h3>Weather in {country.capital[0]}</h3>
+                    <p>Temperature: {weather.main.temp} Celsius</p>
+                    <img
+                        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                        alt={weather.weather[0].description}
+                    />
+                    <p>Wind: {weather.wind.speed} m/s</p>
+                </div>
+
+            )}
         </div>
     )
 }
